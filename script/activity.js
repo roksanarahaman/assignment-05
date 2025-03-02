c483b3429856d217544bbe725402b0de94f0adc6
roksanarahaman <roksana.rahaman12@gmail.com>
2025-03-03 02:41:08 +0900

    added clear history

---
+++ script/activity.js
@@ -0,0 +1,7 @@
+document.getElementById('clear-history').addEventListener('click', 
+    function(){
+        // const activityMessage = document.getElementById('message-container').innerText;
+        // console.log(activityMessage);
+        document.getElementById('message-container').innerText = "";
+
+    })
